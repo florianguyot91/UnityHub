65828c8f809f1a7ad8487ca04ee5c2b78c77e477
Guyot Florian <florian.guyot@isi.fraunhofer.de>
2023-09-20 16:52:05 +0200

    Import to GitHub

---
+++ AI Base Project - RED versus GREEN/AI Base Project - RED versus GREEN/Assets/Scripts/player.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    //[SerializeField] float m_TranslationSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //float vInput = Input.GetAxis("Vertical");
+        //transform.position+=transform.forward * m_TranslationSpeed * Time.deltaTime;
+        //transform.Translate(new Vectot3(0,0,m_TranslationSpeed * Time.deltaTime), Space.self);
+    }
+}
